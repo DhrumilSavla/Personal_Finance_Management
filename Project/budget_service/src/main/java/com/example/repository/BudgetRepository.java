1e8c52608f99fee21b4620046bb83f902b5a114d
DhrumilSavla <92745797+DhrumilSavla@users.noreply.github.com>
2025-03-05 22:13:13 +0530

    Delete Project directory

--- Project/budget_service/src/main/java/com/example/repository/BudgetRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.Budget;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BudgetRepository extends JpaRepository<Budget,Long>{
-
-}
--- Project/savings_service/src/main/java/com/example/repository/SavingsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.Savings;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import  java.util.Optional;
-public interface SavingsRepository extends JpaRepository<Savings,Long> {
-    Optional<Savings> findByAccountNumber(String accountNumber);
-}
